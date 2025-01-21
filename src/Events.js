17841d11ed446a9ce16bec51da697fb515171530
anarotgo <anarotgo@yahoo.com>
2025-01-20 16:52:40 -0800

    Updated visuals in stories, gallery, and events. Updated photos of board members.

--- src/Events.js
+++ src/Events.js
@@ -29,7 +29,7 @@ function Events() {
 
                 <div className='event-details'>
                     <span className='event-name'>Info Session #2</span>
-                    <span className='event-description'>Learn more about Sigma Upsilon Mu.</span>
+                    <span className='event-description'>Learn more about Sigma Upsilon Mu</span>
                     <span className='event-time'>6:15 PM @ the TSU</span>
                 </div>
             </div>
@@ -38,13 +38,13 @@ function Events() {
             <div className='event-box'>
                 <div className='event-date'>
                     <span className='day'>28</span>
-                    <span className='month'>January</span>
+                    <span className='month'>February</span>
                     <span className='year'>2025</span>
                 </div>
 
                 <div className='event-details'>
                     <span className='event-name'>Bid Dinner</span>
-                    <span className='event-description'>Invite only.</span>
+                    <span className='event-description'>Invite only</span>
                     <span className='event-time'>7 PM</span>
                 </div>
             </div>
@@ -59,36 +59,36 @@ function Events() {
 
                 <div className='event-details'>
                     <span className='event-name'>Alumni Night</span>
-                    <span className='event-description'>Get to know our alumni.</span>
-                    <span className='event-time'>7:00 PM</span>
-                </div>
-            </div>
-
-            {/*  */}
-            <div className='event-box'>
-                <div className='event-date'>
-                    <span className='day'>2</span>
-                    <span className='month'>November</span>
-                    <span className='year'>2024</span>
-                </div>
-
-                <div className='event-details'>
-                    <span className='event-name'>Workshop</span>
-                    <span className='event-description'>All kinds of workshops.</span>
-                    <span className='event-time'>7:00 PM</span>
+                    <span className='event-description'>Connect and network with our Alumni</span>
+                    <span className='event-time'>6:30 PM</span>
                 </div>
             </div>
 
             {/* Retreat */}
             <div className='event-box'>
                 <div className='event-date'>
-                    <span className='day'>22-24</span>
-                    <span className='month'>November</span>
+                    <span className='day'>2-4</span>
+                    <span className='month'>May</span>
                     <span className='year'>2024</span>
                 </div>
 
                 <div className='event-details'>
-                    <span className='event-name'>SUM Retreat</span>
+                    <span className='event-name'>Spring Retreat</span>
+                    <span className='event-description'>TBD</span>
+                    <span className='event-time'>TBD</span>
+                </div>
+            </div>
+            
+            {/* Banquet */}
+            <div className='event-box'>
+                <div className='event-date'>
+                    <span className='day'>16</span>
+                    <span className='month'>May</span>
+                    <span className='year'>2024</span>
+                </div>
+
+                <div className='event-details'>
+                    <span className='event-name'>Banquet</span>
                     <span className='event-description'>TBD</span>
                     <span className='event-time'>TBD</span>
                 </div>
