e513a777916ea01f8e332db8bd347b8ddf0f806d
anarotgo <anarotgo@yahoo.com>
2025-01-22 23:26:47 -0800

    Updated visuals and added CTAs.

--- src/Events.js
+++ src/Events.js
@@ -2,97 +2,117 @@ import React from 'react'
 import './styles/events.css'
 
 function Events() {
+
+    // Scroll to section with an offset:
+    const scrollToSection = ( sectionId, offset = 40 ) => {
+        const section = document.getElementById( sectionId );
+        if ( section ) {
+            const sectionPosition = section.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = sectionPosition - offset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            })
+        }
+    };
+
     return (
         <div className='section' id='events-section'>
             <span className='section-title'>EVENTS</span>
 
-            {/* Info Sessions */}
-            <div className='event-box'>
-                <div className='event-date'>
-                    <span className='day'>24</span>
-                    <span className='month'>January</span>
-                    <span className='year'>2025</span>
+            <div id='events-container'>
+                {/* Info Sessions */}
+                <div className='event-box'>
+                    <div className='event-date'>
+                        <span className='day'>24</span>
+                        <span className='month'>January</span>
+                        <span className='year'>2025</span>
+                    </div>
+
+                    <div className='event-details'>
+                        <span className='event-name'>Info Session #1</span>
+                        <span className='event-description'>Learn more about Sigma Upsilon Mu</span>
+                        <span className='event-time'>6:15 PM @ the TSU</span>
+                    </div>
+                </div>
+                <div className='event-box'>
+                    <div className='event-date'>
+                        <span className='day'>31</span>
+                        <span className='month'>January</span>
+                        <span className='year'>2025</span>
+                    </div>
+
+                    <div className='event-details'>
+                        <span className='event-name'>Info Session #2</span>
+                        <span className='event-description'>Learn more about Sigma Upsilon Mu</span>
+                        <span className='event-time'>6:15 PM @ the TSU</span>
+                    </div>
                 </div>
 
-                <div className='event-details'>
-                    <span className='event-name'>Info Session #1</span>
-                    <span className='event-description'>Learn more about Sigma Upsilon Mu</span>
-                    <span className='event-time'>6:15 PM @ the TSU</span>
+                {/* Bid Dinner */}
+                <div className='event-box'>
+                    <div className='event-date'>
+                        <span className='day'>28</span>
+                        <span className='month'>February</span>
+                        <span className='year'>2025</span>
+                    </div>
+
+                    <div className='event-details'>
+                        <span className='event-name'>Bid Dinner</span>
+                        <span className='event-description'>Invite only</span>
+                        <span className='event-time'>7 PM</span>
+                    </div>
+                </div>
+
+                {/* Alumni Night */}
+                <div className='event-box'>
+                    <div className='event-date'>
+                        <span className='day'>19</span>
+                        <span className='month'>April</span>
+                        <span className='year'>2025</span>
+                    </div>
+
+                    <div className='event-details'>
+                        <span className='event-name'>Alumni Night</span>
+                        <span className='event-description'>Connect and network with our Alumni</span>
+                        <span className='event-time'>6:30 PM</span>
+                    </div>
+                </div>
+
+                {/* Retreat */}
+                <div className='event-box'>
+                    <div className='event-date'>
+                        <span className='day'>2-4</span>
+                        <span className='month'>May</span>
+                        <span className='year'>2024</span>
+                    </div>
+
+                    <div className='event-details'>
+                        <span className='event-name'>Spring Retreat</span>
+                        <span className='event-description'>TBD</span>
+                        <span className='event-time'>TBD</span>
+                    </div>
+                </div>
+                
+                {/* Banquet */}
+                <div className='event-box'>
+                    <div className='event-date'>
+                        <span className='day'>16</span>
+                        <span className='month'>May</span>
+                        <span className='year'>2024</span>
+                    </div>
+
+                    <div className='event-details'>
+                        <span className='event-name'>Banquet</span>
+                        <span className='event-description'>TBD</span>
+                        <span className='event-time'>TBD</span>
+                    </div>
                 </div>
             </div>
-            <div className='event-box'>
-                <div className='event-date'>
-                    <span className='day'>31</span>
-                    <span className='month'>January</span>
-                    <span className='year'>2025</span>
-                </div>
 
-                <div className='event-details'>
-                    <span className='event-name'>Info Session #2</span>
-                    <span className='event-description'>Learn more about Sigma Upsilon Mu</span>
-                    <span className='event-time'>6:15 PM @ the TSU</span>
-                </div>
-            </div>
+            <button className='btn' onClick={() => scrollToSection( 'footer-section' )}>Join Us!</button>
 
-            {/* Bid Dinner */}
-            <div className='event-box'>
-                <div className='event-date'>
-                    <span className='day'>28</span>
-                    <span className='month'>February</span>
-                    <span className='year'>2025</span>
-                </div>
-
-                <div className='event-details'>
-                    <span className='event-name'>Bid Dinner</span>
-                    <span className='event-description'>Invite only</span>
-                    <span className='event-time'>7 PM</span>
-                </div>
-            </div>
-
-            {/* Alumni Night */}
-            <div className='event-box'>
-                <div className='event-date'>
-                    <span className='day'>19</span>
-                    <span className='month'>April</span>
-                    <span className='year'>2025</span>
-                </div>
-
-                <div className='event-details'>
-                    <span className='event-name'>Alumni Night</span>
-                    <span className='event-description'>Connect and network with our Alumni</span>
-                    <span className='event-time'>6:30 PM</span>
-                </div>
-            </div>
-
-            {/* Retreat */}
-            <div className='event-box'>
-                <div className='event-date'>
-                    <span className='day'>2-4</span>
-                    <span className='month'>May</span>
-                    <span className='year'>2024</span>
-                </div>
-
-                <div className='event-details'>
-                    <span className='event-name'>Spring Retreat</span>
-                    <span className='event-description'>TBD</span>
-                    <span className='event-time'>TBD</span>
-                </div>
-            </div>
-            
-            {/* Banquet */}
-            <div className='event-box'>
-                <div className='event-date'>
-                    <span className='day'>16</span>
-                    <span className='month'>May</span>
-                    <span className='year'>2024</span>
-                </div>
-
-                <div className='event-details'>
-                    <span className='event-name'>Banquet</span>
-                    <span className='event-description'>TBD</span>
-                    <span className='event-time'>TBD</span>
-                </div>
-            </div>
         </div>
     )
 }
--- src/Stories.js
+++ src/Stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles/stories.css'
 
 import { FaInstagram, FaCamera } from 'react-icons/fa'
@@ -10,74 +10,103 @@ import Daniel from './images/Daniel Cazares.jpg'
 import { IoDiamond } from 'react-icons/io5'
 
 function Stories() {
-  return (
-    <div className='section' id='stories-section'>
-        <span className='section-title'>SUCCESS STORIES</span>
-        
-        <div className='story-container'>
-            <div className='profile'>
-                <img src={ Boomer } className='portrait story-portrait' alt='Boomer Baker'/>
-                <span className='story-name'>Boomer Baker</span>
-            </div>
-            <div className='story-details'>
-            <span className='quote'>"You are the SUM of 5 people around you or something like that and I'll be teaching your workshops."</span>
-                <ul className='story-achievements'>
-                    <li className='story-achievement'>CEO of Neuralquant</li>
-                    <li className='story-achievement'>Investor in Ayli, Bioletic, & Think 66</li>
-                    <li className='story-achievement'>Made $2+ billions in sales</li>
-                </ul>
-                {/* <div className='icons'>
-                    <a href='none'><FaLinkedin className='icon'/></a>
-                    <a href='none'><FaInstagram className='icon'/></a>
-                    <a href='none'><FaEnvelopeSquare className='icon'/></a>
-                </div> */}
-            </div>
-        </div>
+    
+    // Scroll to section with an offset:
+    const scrollToSection = ( sectionId, offset = 40 ) => {
+        const section = document.getElementById( sectionId );
+        if ( section ) {
+            const sectionPosition = section.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = sectionPosition - offset;
 
-        <div className='story-container'>
-            <div className='profile'>
-                <img src={ Daniel } className='portrait story-portrait' alt='Daniel Cazares'/>
-                <span className='story-name'>Daniel Cazares</span>
-            </div>
-            <div className='story-details'>
-                <span className='quote'>"Being involved may be just as important or more important than your degree."</span>
-                <ul className='story-achievements'>
-                    <li className='story-achievement'>Co-Owner of Don Victor Jewelers</li>
-                    <li className='story-achievement'>Made it on the Inc. 5000 list for fastest growing companies in America</li>
-                    <li className='story-achievement'>Made multiple 7 figures</li>
-                </ul>
-                <div className='icons'>
-                    <a href='https://donvictorjewelers.com/pages/about-us' target='_blank' rel='noopener noreferrer'>
-                        <IoDiamond className='icon' />
-                    </a>
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            })
+        }
+    };
+
+    const [ctaText, setCtaText] = useState("Want to be Featured?")
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCtaText((prevText) =>
+                prevText === "Want to be Featured?" ? "Ready to be SUMone?" : "Want to be Featured?"
+            )
+        }, 1500)
+
+        return () => clearInterval(interval)
+    })
+
+    return (
+        <div className='section' id='stories-section'>
+            <span className='section-title'>SUCCESS STORIES</span>
+            
+            <div id='stories-container'>
+                <div className='story-container'>
+                    <div className='profile'>
+                        <img src={ Boomer } className='portrait story-portrait' alt='Boomer Baker'/>
+                        <span className='story-name'>Boomer Baker</span>
+                    </div>
+                    <div className='story-details'>
+                    <span className='quote'>"You are the SUM of 5 people around you or something like that and I'll be teaching your workshops."</span>
+                        <ul className='story-achievements'>
+                            <li className='story-achievement'>CEO of Neuralquant</li>
+                            <li className='story-achievement'>Investor in Ayli, Bioletic, & Think 66</li>
+                            <li className='story-achievement'>Made $2+ billions in sales</li>
+                        </ul>
+                        {/* <div className='icons'>
+                            <a href='none'><FaLinkedin className='icon'/></a>
+                            <a href='none'><FaInstagram className='icon'/></a>
+                            <a href='none'><FaEnvelopeSquare className='icon'/></a>
+                        </div> */}
+                    </div>
+                </div>
+
+                <div className='story-container'>
+                    <div className='profile'>
+                        <img src={ Daniel } className='portrait story-portrait' alt='Daniel Cazares'/>
+                        <span className='story-name'>Daniel Cazares</span>
+                    </div>
+                    <div className='story-details'>
+                        <span className='quote'>"Being involved may be just as important or more important than your degree."</span>
+                        <ul className='story-achievements'>
+                            <li className='story-achievement'>Co-Owner of Don Victor Jewelers</li>
+                            <li className='story-achievement'>Made it on the Inc. 5000 list for fastest growing companies in America</li>
+                            <li className='story-achievement'>Made multiple 7 figures</li>
+                        </ul>
+                        <div className='icons'>
+                            <a href='https://donvictorjewelers.com/pages/about-us' target='_blank' rel='noopener noreferrer'>
+                                <IoDiamond className='icon' />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='story-container'>
+                    <div className='profile'>
+                        <img src={ Erick } className='portrait story-portrait' alt='Erick R. OjedaGarcia'/>
+                        <span className='story-name'>Erick R. OjedaGarcia</span>
+                    </div>
+                    <div className='story-details'>
+                        <span className='quote'>"If you’re ready to challenge yourself, build lifelong connections, and unlock new opportunities, I highly recommend joining Sigma Upsilon Mu! ... it’s a launchpad for future leaders, innovators, and game-changers."</span>
+                        <ul className='story-achievements'>
+                            <li className='story-achievement'>Mayor of Wilmington, CA</li>
+                            <li className='story-achievement'>CEO of OMGee PIX! LLC, a community-oriented business supporting local nonprofits and events.</li>
+                            <li className='story-achievement'>Created the Wilmington California Information Network, connecting residents with businesses, nonprofits, and local resources.</li>
+                            {/* <li className='story-achievement'>Veteran of the Year</li> */}
+                        </ul>
+                        <div className='icons'>
+                            <a href='https://omgeepix.com/'><FaCamera className='icon'/></a>
+                            <a href='https://www.instagram.com/omgeepix/?hl=en'><FaInstagram className='icon'/></a>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            <button className='btn' id='stories-cta-btn' onClick={() => scrollToSection( 'footer-section' )}>{ctaText}</button>
+
         </div>
-
-        <div className='story-container'>
-            <div className='profile'>
-                <img src={ Erick } className='portrait story-portrait' alt='Erick R. OjedaGarcia'/>
-                <span className='story-name'>Erick R. OjedaGarcia</span>
-            </div>
-            <div className='story-details'>
-                <span className='quote'>"If you’re ready to challenge yourself, build lifelong connections, and unlock new opportunities, I highly recommend joining Sigma Upsilon Mu! ... it’s a launchpad for future leaders, innovators, and game-changers."</span>
-                <ul className='story-achievements'>
-                    <li className='story-achievement'>Mayor of Wilmington, CA</li>
-                    <li className='story-achievement'>CEO of OMGee PIX! LLC, a community-oriented business supporting local nonprofits and events.</li>
-                    <li className='story-achievement'>Created the Wilmington California Information Network, connecting residents with businesses, nonprofits, and local resources.</li>
-                    {/* <li className='story-achievement'>Veteran of the Year</li> */}
-                </ul>
-                <div className='icons'>
-                    <a href='https://omgeepix.com/'><FaCamera className='icon'/></a>
-                    <a href='https://www.instagram.com/omgeepix/?hl=en'><FaInstagram className='icon'/></a>
-                </div>
-            </div>
-        </div>
-
-        <span>and many more!</span>
-
-    </div>
-  )
+    )
 }
 
 export default Stories
