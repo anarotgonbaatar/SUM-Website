3edfb900ed6f474e19c71da723da27d2c087fa0c
anarotgo <anarotgo@yahoo.com>
2025-01-23 18:31:57 -0800

    About section updated

--- src/About.js
+++ src/About.js
@@ -1,43 +1,55 @@
 import React from 'react'
 import './styles/about.css'
+import Phoenix from './images/phoenix-crimson.png'
 
 function About() {
     return (
         <div className='section' id='about-section'>
+
+            {/* Background Phoenix */}
+            <div className='background-phoenix' id='about-phoenix' style={{backgroundImage: `url(${Phoenix})`}}></div>
             
             <span className='section-title'>ABOUT</span>
-            <p className='about-p'>
+            <p className='about-p glass-card about-card'>
                 Sigma Upsilon Mu (SUM) is a co-ed entrepreneurship business fraternity established in March 2014.
             </p>
             
-            <span className='about-header'>Our Motto</span>
-            <span id='motto'>"Facta Non Verba" (Deeds, Not Words)</span>
-            <p className='about-p'>This reflects our belief in taking action over words, transforming the lives of college students to help them reach their fullest potential.</p>
+            <div className='glass-card about-card'>
+                <span className='about-header'>Our Motto</span>
+                <span id='motto'>"Facta Non Verba" (Deeds, Not Words)</span>
+                <p className='about-p'>This reflects our belief in taking action over words, transforming the lives of college students to help them reach their fullest potential.</p>
+            </div>
             
-            <span className='about-header'>What We Do</span>
-            <ul>
-                <li>We empower students to become lifelong learners and future entrepreneurs, executives, and financially independent individuals.</li>
-                <li>We organize a variety of events each semester, such as:</li>
-                <li>8-hour business challenges</li>
-                <li>Networking events</li>
-                <li>Guest speaker sessions from industry professionals</li>
-                <li>Workshops and hands-on entrepreneurial projects</li>
-            </ul>
+            <div className='glass-card about-card'>
+                <span className='about-header'>What We Do</span>
+                <ul>
+                    <li>We empower students to become lifelong learners and future entrepreneurs, executives, and financially independent individuals.</li>
+                    <li>We organize a variety of events each semester, such as:</li>
+                    <li>8-hour business challenges</li>
+                    <li>Networking events</li>
+                    <li>Guest speaker sessions from industry professionals</li>
+                    <li>Workshops and hands-on entrepreneurial projects</li>
+                </ul>
+            </div>
 
-            <span className='about-header'>Our Process</span>
-            <ul>
-                <li>New members go through a pledging process and join a chapter class.</li>
-                <li>Members participate in workshops, projects, and events that teach entrepreneurship.</li>
-                <li>They apply the principles learned during the semester to build businesses or pursue career paths aligned with their goals.</li>
-            </ul>
+            <div className='glass-card about-card'>
+                <span className='about-header'>Our Process</span>
+                <ul>
+                    <li>New members go through a pledging process and join a chapter class.</li>
+                    <li>Members participate in workshops, projects, and events that teach entrepreneurship.</li>
+                    <li>They apply the principles learned during the semester to build businesses or pursue career paths aligned with their goals.</li>
+                </ul>
+            </div>
 
-            <span className='about-header'>Our 4 Pillars: A.B.L.E.</span>
-            <ul>
-                <li>Achievement</li>
-                <li>Brotherhood</li>
-                <li>Leadership</li>
-                <li>Entrepreneurship</li>
-            </ul>
+            <div className='glass-card about-card'>
+                <span className='about-header'>Our 4 Pillars: A.B.L.E.</span>
+                <div id='pillars'>
+                    <p className='pillar'>Achievement</p>
+                    <p className='pillar'>Brotherhood</p>
+                    <p className='pillar'>Leadership</p>
+                    <p className='pillar'>Entrepreneurship</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Mission.js
+++ src/Mission.js
@@ -10,7 +10,7 @@ function Mission() {
             <div className='background-phoenix' style={{backgroundImage: `url(${Phoenix})`}}></div>
 
             <span className='section-title'>OUR MISSION</span>
-            <p>
+            <p className='glass-card'>
                 We believe that human potential is limitless, never settling for a mediocre life, and that actions speak louder than words.
                 Through a continuous cycle of high achievement, brotherhood, leadership, and entrepreneurship principles, we create great members, that join great companies, that redefine the limits of what’s possible.
                 Through deeds not words, together, our sole mission is to continuously cultivate an ecosystem that develops the highest achievers in the world.
